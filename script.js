6dc8f8d4c6ac2f2ac5812fc205a8b2aaed9b5f5a
space-infornations <kartikmonga217@gmail.com>
2025-09-02 20:03:35 +0530

    new  update

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ window.onscroll = function () {
 goTopBtn.addEventListener("click", function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+// gallery new
+
+// Projects Gallery Lightbox (no sequence)
+document.querySelectorAll(".gallery-card a").forEach(link => {
+  link.addEventListener("click", function () {
+    const imgSrc = this.querySelector("img").getAttribute("src");
+    document.getElementById("lightboxImage").setAttribute("src", imgSrc);
+  });
+});
+
+// Logo Gallery Lightbox (no sequence)
+document.querySelectorAll(".logo-gallery-item a").forEach(link => {
+  link.addEventListener("click", function () {
+    const imgSrc = this.querySelector("img").getAttribute("src");
+    document.getElementById("logoLightboxImage").setAttribute("src", imgSrc);
+  });
+});
